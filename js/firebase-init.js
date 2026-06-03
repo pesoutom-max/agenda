e28a6de998ec47cc28f3c1459e39f6051ddae71b
Eduardo Souto <pesoutom@gmail.com>
2026-06-03 17:48:37 -0400

    fix: embed firebase config fallback

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,7 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const app = initializeApp(window.firebaseConfig);
+const firebaseConfig = window.firebaseConfig || {
+  apiKey: "AIzaSyAg_FZL9SljS9RsaiXXRHCTt4L3ZXOl20Q",
+  authDomain: "facilpyme-agenda.firebaseapp.com",
+  projectId: "facilpyme-agenda",
+  storageBucket: "facilpyme-agenda.firebasestorage.app",
+  messagingSenderId: "256698206822",
+  appId: "1:256698206822:web:bc4c254c156f0833463ba7"
+};
+
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db, app };
